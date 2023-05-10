15b3439e3742aeca4b929cfdead098f293e146ad
Alecks Gates <agates@mail.agates.io>
2023-05-10 08:06:43 -0500

    Add support for feed generation using the Podcast Namespace (#1)
    * add custom fields
    
    * adding torrent enclosure support
    
    * added support for torrent via arrays/objects/MRSS
    
    * added babel support for rest operator and target node 8 (as used in peertube)
    
    * hotfix: adding proper namespaces and better torrent object handling
    
    1.1.4
    
    * adding support for media:group and moving from peerLink to more complete media:content
    
    1.1.5
    
    * adding support for media:thumbnail
    
    1.1.6
    
    * fixing spec inconsitencies with image enclosure and mrss
    
    - adding mandatory attribute to enclosure image, which fixes #2
    - adding missing namespace when using media:thumbnail, which fixes #3
    
    1.1.7
    
    * (fix) increment package version
    
    * (mrss) implement additional properties in the item scope for #10
    
    - media:categories
    - media:community
      - media:statistics
      - media:starRating
    - media:keywords
    - media:embed
    - media:player
    - media:subTitle
    
    * (lint) reformatting code for readability
    
    using prettier
    
    * support rich group properties, fix peerLink
    
    BREAKING: torrent was considered as media:content instead of the specified
    media:peerLink
    
    resolves #10
    
    1.1.9
    
    * (fix) increment package version
    
    * (fix) hash property
    
    * (fix) deal with empty category value
    
    1.1.11
    
    * Update babel/jest
    
    * Add initial podcast namespace support
    
    * Change package for test publishing on NPM
    
    * Re-add content namespace
    
    * 1.2.2
    
    * Fix itunes:image to use href attribute on podcast
    
    * 1.2.3
    
    * Replace title attribute with optional height
    
    Title was mostly meaningless and height is much more obvious while still optional.
    
    * 1.3.0
    
    * Update gitignore for intellij/webstorm
    
    * Add base .idea project files
    
    * 1.4.0
    
    * Add managingEditor, webMaster, itunes:owner
    
    * Add socialInteract, medium
    
    * Update dependencies
    
    * Start version 1.5.0
    
    * Add support for podcast:liveItem and experimental podcast:tracker
    
    * 1.5.1
    
    * Add title attribute to alternateEnclosure
    
    * 1.5.2
    
    * Add podcast:locked tag
    
    * Add experimental podcast:categories on channel and items
    
    * 1.6.0
    
    * Don't generate categories with empty list
    
    * 1.6.1
    
    * Initial commit for podcast feeds using The Podcast Namespace
    
    * Remove old JS files
    
    * Add support for custom tags not built in
    
    * Fix person/locked/explicit
    
    * Version 6.1.1 for packaging changes
    
    * Update sanitized attributes
    
    * Version 6.1.2
    
    * Revert package.json for upstream merge
    
    * Remove .idea folder
    
    * Remove old package-lock.json
    
    * Remove extra files
    
    * Remove .idea folder
    
    * Remove old package-lock.json
    
    * Remove extra files
    
    * alternateEnclosure language attribute should be "lang"
    
    * alternateEnclosure language attribute should be "lang"
    
    * Add support for custom XMLNS definitions
    
    * Add STUN server listing for podcast feeds, add trackers to channel
    
    ---------
    
    Co-authored-by: Rigel Kent <sendmemail@rigelk.eu>

--- src/typings/index.ts
+++ src/typings/index.ts
@@ -76,6 +76,60 @@ export interface Item {
   nsfw?: boolean
 }
 
+export interface PodcastItem {
+  title: string
+  id?: string
+  link: string
+  date: Date
+
+  description?: string
+  content?: string
+  category?: Category[]
+
+  guid?: string
+
+  media: Media[]
+
+  author?: Author[]
+  person?: Person[]
+
+  published?: Date
+  copyright?: string
+
+  subTitle?: {
+    url: string
+    type: string
+    language?: string
+    rel?: string
+  }[]
+
+  thumbnails?: {
+    url: string
+    width?: number
+  }[]
+
+  stunServers?: string[]
+  trackers?: string[]
+
+  socialInteract?: SocialInteract[]
+
+  nsfw?: boolean
+
+  customTags?: CustomTag[]
+}
+
+export interface PodcastLiveItem extends PodcastItem {
+  status: LiveItemStatus
+  start?: string
+  end?: string
+}
+
+export enum LiveItemStatus {
+  pending = "pending",
+  live = "live",
+  ended = "ended",
+}
+
 export interface Enclosure {
   url: string
   type?: string
@@ -84,10 +138,56 @@ export interface Enclosure {
   duration?: number
 }
 
+export interface Media {
+  type: string
+  codecs?: string
+  length?: number
+  bitrate?: number
+  height?: number
+  title?: string
+  rel?: string
+  language?: string
+
+  sources: Source[]
+
+  integrity?: SourceIntegrity[]
+}
+
+export interface Source {
+  uri: string
+  contentType?: string
+}
+
+export interface SourceIntegrity {
+  type: IntegrityType
+  value: string
+}
+
+export enum IntegrityType {
+  sri = "sri",
+  pgpSignature = "pgp-signature"
+}
+
+export interface SocialInteract {
+  uri: string
+  protocol: string
+  accountId?: string
+  accountUrl?: string
+  priority?: number
+}
+
 export interface Author {
   name?: string
   email?: string
   link?: string
+}
+
+export interface Person {
+  name: string
+  href?: string
+  img?: string
+  role?: string
+  group?: string
 }
 
 export interface Category {
@@ -98,7 +198,8 @@ export interface Category {
 }
 
 export interface FeedOptions {
-  id: string
+  id?: string
+  guid?: string
   title: string
   updated?: Date
   generator?: string
@@ -109,16 +210,40 @@ export interface FeedOptions {
   feedLinks?: any
   hub?: string
   docs?: string
+  medium?: string
 
   author?: Author
+  managingEditor?: Author,
+  webMaster?: Author,
+  person?: Person[]
+  locked?: { isLocked: boolean, email: string }
   link?: string
   description?: string
   image?: string
   favicon?: string
   copyright: string
+  nsfw?: boolean,
+
+  stunServers?: string[]
+  trackers?: string[]
+
+  customXMLNS?: CustomXMLNS[]
+  customTags?: CustomTag[]
 }
 
 export interface Extension {
   name: string
   objects: any
+}
+
+export interface CustomXMLNS {
+  name: string,
+  value: string
+}
+
+export interface CustomTag {
+  name: string
+  attributes?: { [key: string]: string }
+  value?: string | CustomTag[]
+  cdata?: boolean
 }
